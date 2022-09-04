4742e21d05691f78fe5769ce18598319944f7295
Lucas Veras <lucasverasaquino@gmail.com>
2022-09-04 15:23:12 -0300

    Reajustes validação formulário

--- script.js
+++ script.js
@@ -10,32 +10,37 @@ form.addEventListener("submit", (event) => {
     //evitando que a ação padrão de atualizar a página ao clicar em submit ocorra
     event.preventDefault()
 
-    //chamando a função de checar os inputs quando o botão submit for clicado
+    //chamando a função de checar os inputs quando o botão submit for clicado (evento)
     checkInputs()
 })
 
 //Função que checará se os inputs estão preenchidos corretamente
 function checkInputs(){
 
-    //Pegando os valores dos inputs para a validação
+    //Pegando os values dos inputs para a validação
     const firstNameValue = firstName.value
     const lastNameValue  = lastName.value
     const emailValue     = email.value
     const passwordValue  = password.value
 
-
     //validação firstName
     if (firstNameValue === ""){
         errorInput(firstName, "First Name cannot be empty")
-    } else {
-        successInput(firstName)
+    } else if(firstNameValue.length < 5){
+        //validação caso o value First name seja menor que 5 (evitar envios vazios)
+        errorInput(firstName, "First Name must have at least 5 characters")
+    } else{ 
+        validInput(firstName)
     }
 
     //validação lastName 
     if (lastNameValue === ""){
         errorInput(lastName, "Last Name cannot be empty")
+    } else if(lastNameValue.length < 5){
+        //validação caso o value last name seja menor que 5 (evitar envios vazios)
+        errorInput(lastName, "Last Name must have at least 5 characters")
     } else {
-        successInput(lastName)
+        validInput(lastName)
     }
 
     //validação email, onde o else if puxa o regex para validar os caracteres
@@ -51,19 +56,18 @@ function checkInputs(){
         const emailExample = email.parentElement
         //alterando as classes do elemento pai com shadow-DOM
         emailExample.className = "form-control email-exemple error"
-
     } else {
-        successInput (email)
+        validInput (email)
     }
 
+    //Validação password onde o else if válida o tamanho mínimo da senha
     if (passwordValue === "") {
         errorInput(password, "Password cannot be empty")
     } else if (passwordValue.length < 8) {
         errorInput(password, "Password must have at least 8 characters")
     } else {
-        successInput(password)
+        validInput(password)
     }
-
 }
 
 //Função que executará os erros de validação
@@ -73,25 +77,19 @@ function errorInput (input, message) {
     //pegando a mensagem de erro para alterá-la de acordo com o erro
     const small = formControl.querySelector("small")
 
+    //Alterando a mensagem de erro e adicionando a classe de erro
     small.innerText = message
-
     formControl.className = "form-control error"
 }
 
-function successInput (input) {
+//Função que fará os inputs voltarem ao estado normal após validação dos values
+function validInput (input) {
     const formControl = input.parentElement
     const small = formControl.querySelector("small")
+
     small.innerText = ""
     formControl.className = "form-control"
-
 }
-
-
-
-
-
-
-
 
 //Regex para email
 function checkEmail(email) {
